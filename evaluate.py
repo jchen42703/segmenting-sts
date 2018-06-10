8e41fba949451d5bdd56191f98a0fe42510f5237
Joseph Chen <34604336+jchen42703@users.noreply.github.com>
2018-06-10 17:41:31 -0400

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,21 +1,14 @@
-
-# coding: utf-8
-
-# In[2]:
-
-
 from keras.models import load_model
 model_path = '...'
 
-
-# In[ ]:
-
-
 model = load_model(model_path)
+
+#load data
 import numpy as np 
 ct_test = np.load('ct_test.npy')
 y_test = np.load('y_patches_test.npy')
 
+#evaluation and prediction
 model.evaluate(ct_test, y_test)
 predict = model.predict(ct_test)
 
