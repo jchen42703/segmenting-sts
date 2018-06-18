080eaae5c39802555da028b4d3d343b6dce525cc
Joseph Chen <34604336+jchen42703@users.noreply.github.com>
2018-06-18 08:50:14 -0400

    Update model.py

--- models/model.py
+++ models/model.py
@@ -7,6 +7,69 @@ from keras.engine import Layer
 from keras.models import *
 import keras.backend as K
 
+#credits to dalmia for the code for this layer
+class MaxoutConv2D(Layer):
+    """
+    Convolution Layer followed by Maxout activation as described 
+    in https://arxiv.org/abs/1505.03540.
+    
+    PARAMETERS
+    ----------
+    
+    kernel_size: kernel_size parameter for Conv2D
+    output_dim: final number of filters after Maxout
+    keep_prob: keep probability for Dropout
+    nb_features: number of filter maps to take the Maxout over; default=4
+    padding: 'same' or 'valid'
+    first_layer: True if x is the input_tensor
+    input_shape: Required if first_layer=True
+    
+    """
+    
+    def __init__(self, kernel_size, output_dim, nb_features=4, padding='valid', **kwargs):
+        
+        self.kernel_size = kernel_size
+        self.output_dim = output_dim
+        self.nb_features = nb_features
+        self.padding = padding
+        super(MaxoutConv2D, self).__init__(**kwargs)
+
+    def call(self, x):
+
+        num_channels = self.output_dim * self.nb_features
+        conv_out = Conv2D(num_channels, self.kernel_size, padding=self.padding)(x)
+        batch_norm_out = BatchNormalization()(conv_out)
+        out_shape = batch_norm_out.get_shape().as_list()
+        reshape_out = Reshape((out_shape[1], out_shape[2], 
+                               self.nb_features, self.output_dim))(batch_norm_out)
+        maxout_out = K.max(reshape_out, axis=-2)
+
+        return maxout_out
+
+    def get_config(self):
+
+        config = {"kernel_size": self.kernel_size,
+                  "output_dim": self.output_dim,
+                  "nb_features": self.nb_features,
+                  "padding": self.padding}
+
+        base_config = super(MaxoutConv2D, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    def compute_output_shape(self, input_shape):
+        input_height= input_shape[1]
+        input_width = input_shape[2]
+        
+        if(self.padding == 'same'):
+            output_height = input_height
+            output_width = input_width
+        
+        else:
+            output_height = input_height - self.kernel_size[0] + 1
+            output_width = input_width - self.kernel_size[1] + 1
+        
+        return (input_shape[0], output_height, output_width, self.output_dim)
+
 #defining models architecture
 def UNET_2D(x_train): 
     '''
@@ -46,164 +109,55 @@ def UNET_2D(x_train):
 
     return model
 
-class MultichannelCascadeCNN(object):
-    """
-    from : https://github.com/dalmia/Brain-Tumor-Segmentation-Keras
+def cascade(first_cascade, second_cascade):
+    ''' 
+    Arguments:
+        first_cascade: input array for the first cascade (56 by 56 patches)
+        second_cascade: input array for the second cascade (28 by 28 patches)
+        NOTE: TO GET THE SECOND_CASCADE PATCHES, JUST CHANGE THE PATCH DIMENSIONS IN preprocess.py and run gen_patches
+    Returns:
+        model: keras class 
+    '''
     
-    A Multi-channel cascaded CNN architecture introduced in 
-    https://arxiv.org/pdf/1505.03540.pdf - "Brain Tumor Segmentation with Deep Neural Networks".
+    #first cascade
+    first_cascade_input = Input(first_cascade.shape[1:])
+    conv_1 = MaxoutConv2D(kernel_size = (7,7), output_dim=64, name = 'conv_1')(first_cascade_input)
+    maxpool_1 = MaxPooling2D(pool_size=4, strides=(1, 1), 
+                                     name='local_pool1')(conv_1)
     
-    PARAMETERS
-    ----------
-    patch_size: int, default=33
-                Size of the patch fed into the model.
+    conv_2 = MaxoutConv2D(kernel_size = (3,3), output_dim=64, name = 'conv_2')(maxpool_1)
+    maxpool_2 = MaxPooling2D(pool_size=2, strides=(1, 1), 
+                                     name='local_pool2')(conv_2)
+   
+    global_output = MaxoutConv2D((13,13), output_dim=160, padding = 'same',
+                                     name='global')(maxpool_2)
     
-    mode: {'input', 'local', 'final'}, default='input'
-          The type of cascaded to be done (Refer the README for clarity).
-          
-    num_channels: int, default=1
-                  Number of channels in the input patch.
+    output = Concatenate(axis=-1, name='concat')([maxpool_2, global_output])
+    output = Conv2D(2, (17,17), padding='valid', name='conv_last')(output)
+    output = Activation('softmax', name='softmax')(output)   
     
-    num_classes: int, default=2
-                 Number of possible classes for a pixel.
-                 
-    num_filters_local_1: int, default=64
-                         Number of filters to be used in the first convolutional layer
-                         of the local pathway.
-                         
-    num_filters_local_2: int, default=64
-                         Number of filters to be used in the second convolutional layer
-                         of the local pathway.
-                         
-    num_filters_global: int, default=160
-                         Number of filters to be used in the convolutional layer of the
-                         global pathway.
-                         
-    kernel_local_1: int, default=7
-                    Kernel size to be used in the first convolutional layer of the local
-                    pathway.
-                    
-    kernel_local_2: int, default=3
-                    Kernel size to be used in the second convolutional layer of the local
-                    pathway.
-                
-    kernel_global: int, default=13
-                   Kernel size to be used in the convolutional layer of the global pathway.
+    #second cascade (input mode)
+    second_cascade_input = Input(second_cascade.shape[1:])
+    final_input = Concatenate(axis=-1, name='input_concat')([second_cascade_input, output])
+    conv2_1 = MaxoutConv2D(kernel_size = (7,7), output_dim=64, name = '2_conv_1')(final_input)
+    maxpool2_1 = MaxPooling2D(pool_size=4, strides=(1, 1), 
+                                     name='2_local_pool1')(conv2_1)
     
-    pool_local_1: int, default=4
-                  The pooling size for the max pooling layer after the first convolutional 
-                  layer of the local pathway.
+    conv2_2 = MaxoutConv2D(kernel_size = (3,3), output_dim=64, name = '2_conv_2')(maxpool2_1)
+    maxpool2_2 = MaxPooling2D(pool_size=2, strides=(1, 1), 
+                                     name='2_local_pool2')(conv2_2)
+   
+    global2_output = MaxoutConv2D((13,13), output_dim=160, padding = 'same',
+                                     name='2_global')(maxpool2_2)
     
-    pool_local_2: int, default=2
-                  The pooling size for the max pooling layer after the second convolutional 
-                  layer of the local pathway.
+    output2 = Concatenate(axis=-1, name='2_concat')([maxpool2_2, global2_output])
+    output2 = Conv2D(2, (16,16), padding='valid', name='2_conv_last')(output2)
+    output2 = Activation('softmax', name='2_softmax')(output2)   
     
+    final_output = Flatten()(output2)
+    model = Model(inputs=[first_cascade_input, second_cascade_input], outputs=final_output)
     
-    """
-    
-    def __init__(self, patch_size=33, mode='input', num_channels=1, num_classes=2, 
-                 num_filters_local_1=64, num_filters_local_2=64, num_filters_global=160, 
-                 kernel_local_1=7, kernel_local_2=3, kernel_global=13, pool_local_1=4,
-                 pool_local_2=2):
-        
-        self.patch_size = patch_size
-        self.mode = mode
-        self.num_channels = num_channels
-        self.num_classes = num_classes
-        self.num_filters_local_1 = num_filters_local_1
-        self.num_filters_local_2 = num_filters_local_2
-        self.num_filters_global = num_filters_global
-        self.kernel_local_1 = kernel_local_1
-        self.kernel_local_2 = kernel_local_2
-        self.kernel_global = kernel_global
-        self.pool_local_1 = pool_local_1
-        self.pool_local_2 = pool_local_2
-        
-        self.classification_kernel_size = self.patch_size - self.kernel_global + 1
-        
-        if(self.mode == 'input'):
-            self.outer_patch_size = 2 * self.patch_size - 1
-            
-        elif(self.mode == 'local'):
-            self.outer_patch_size = self.patch_size + self.classification_kernel_size + \
-                self.pool_local_2 + self.kernel_local_2 - 3
-        
-        else:
-            self.outer_patch_size = self.patch_size + self.classification_kernel_size - 1
-            
-            
-    def _forward_pass(self, model_input, stage, append_output=None, prev_output=None):
-        
-        local_kernel_1 = (self.kernel_local_1, self.kernel_local_1)
-        local_kernel_2 = (self.kernel_local_2, self.kernel_local_2)
-        global_kernel = (self.kernel_global, self.kernel_global)
-
-        local_output_dim_1 = self.num_filters_local_1
-        local_output_dim_2 = self.num_filters_local_2
-        global_output_dim = self.num_filters_global
-
-        local_pool_1 = (self.pool_local_1, self.pool_local_1)
-        local_pool_2 = (self.pool_local_2, self.pool_local_2)
-        
-        # InputCascadeCNN
-        if(append_output == 'input'): 
-            final_input = Concatenate(axis=-1, name='input_concat')([model_input, prev_output])
-
-        else:
-            final_input = model_input
-
-        local_output = MaxoutConv2D(local_kernel_1, output_dim=local_output_dim_1, 
-                                    name=stage+'_local_conv1')(final_input)
-
-        local_output = MaxPooling2D(pool_size=local_pool_1, strides=(1, 1),
-                                    name=stage+'_local_pool1')(local_output)
-        
-        # LocalCascadeCNN
-        if(append_output == 'local'):
-            local_output = Concatenate(axis=-1, name='local_concat')([local_output, prev_output])
-
-        local_output = MaxoutConv2D(local_kernel_2, output_dim=local_output_dim_2, 
-                                    name=stage+'_local_conv2')(local_output)
-        local_output = MaxPooling2D(pool_size=local_pool_2, strides=(1, 1), 
-                                    name=stage+'_local_pool2')(local_output)
-
-        global_output = MaxoutConv2D(global_kernel, output_dim=global_output_dim, 
-                                     name=stage+'_global')(final_input)
-
-        output = Concatenate(axis=-1, name=stage+'_concat')([local_output, global_output])      
-        
-    def build_model(self):
-        """
-        Builds the MultiCascadedCNN model.
-        
-        Returns
-        -------
-        
-        model: Keras Model object
-               The MultiCascadedCNN model as per the mode.
-               
-        """
-        
-        # First stage
-        first_cascade_input_shape = (self.outer_patch_size, 
-                                     self.outer_patch_size, 
-                                     self.num_channels)
-    
-        first_cascade_input = Input(shape=first_cascade_input_shape)
-        first_cascade_output = self._forward_pass(first_cascade_input, stage='1')    
-        
-        # Second stage
-        second_cascade_input_shape = (self.patch_size, self.patch_size, self.num_channels)
-        second_cascade_input = Input(shape=second_cascade_input_shape)
-        second_cascade_output = self._forward_pass(second_cascade_input, 
-                                             stage='2',
-                                             append_output=self.mode,
-                                             prev_output=first_cascade_output)
-
-        output = Flatten()(second_cascade_output)
-        model = Model(inputs=[first_cascade_input, second_cascade_input], outputs=output)
-        
-        return model
+    return model
 
 def dilated(x_train):
     '''
