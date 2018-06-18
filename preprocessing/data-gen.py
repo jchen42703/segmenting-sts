4dde980bd93bc506b20a1192deb71fc51a243935
Joseph Chen <34604336+jchen42703@users.noreply.github.com>
2018-06-18 08:53:39 -0400

    Update data-gen.py

--- preprocessing/data-gen.py
+++ preprocessing/data-gen.py
@@ -14,6 +14,6 @@ pet_slices = pch.normalize(pch.get_slices(pch.pet_data, patient_ids = pch.patien
 test_ct_slices = pch.normalize(pch.get_slices(pch.ct_data, patient_ids = pch.patients['test'], use_pos_window = True))
 test_pet_slices = pch.normalize(pch.get_slices(pch.pet_data, patient_ids = pch.patients['test'], use_pos_window = True))
 
-pch.get_patches(ct_slices, pet_slices, save = True)
-pch.get_patches(test_ct_slices, test_pet_slices, save = True)
+pch.gen_patches(ct_slices, pet_slices, save = True)
+pch.gen_patches(test_ct_slices, test_pet_slices, save = True)
 pch.get_patch_labels(pch.y_data, save = True)
